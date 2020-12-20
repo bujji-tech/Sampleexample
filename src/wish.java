5a2868646c77e94a016975260b4040d30e60fdca
siva-cloud <dvsivap@gmail.com>
2020-12-20 20:50:36 +0530

    wish-2323

---
+++ src/wish.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+
+public class wish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] list1= {1,2,3,4,5};
+		int[] list2= {1,6,7,5,8};
+		 for ( int i=0; i< list1.length;i++)
+		 for (int j=0; j<list2.length;j++)
+		 if(list1[i]==list2[j]) {
+			 System.out.println("commen elements is: "+list1[i]);
+		 }
+			 
+		
+	}
+
+}
